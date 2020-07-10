7c009818ef2dd169d27173c8d26bc85ab5aa0d69
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2020-07-10 17:19:09 +0200

    When exporting function use GWL_PUBLIC

--- include/gwl-import.h
+++ include/gwl-import.h
@@ -1,17 +1,16 @@
 #pragma once
 #if defined _WIN32 || defined __CYGWIN__
-  #ifdef BUILDING_GWAYLAND
-    #define GWAYLAND_PUBLIC __declspec(dllexport)
+  #ifdef BUILDING_GWL
+    #define GWL_PUBLIC __declspec(dllexport)
   #else
-    #define GWAYLAND_PUBLIC __declspec(dllimport)
+    #define GWL_PUBLIC __declspec(dllimport)
   #endif
 #else
-  #ifdef BUILDING_GWAYLAND
-      #define GWAYLAND_PUBLIC __attribute__ ((visibility ("default")))
+  #ifdef BUILDING_GWL
+      #define GWL_PUBLIC __attribute__ ((visibility ("default")))
   #else
-      #define GWAYLAND_PUBLIC
+      #define GWL_PUBLIC
   #endif
 #endif
 
-int GWAYLAND_PUBLIC gwa_func();
 
